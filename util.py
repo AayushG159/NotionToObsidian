14feba780337561b87a48a0cadb32a144044eaee
Aayush Gupta <gaayushmaan@gmail.com>
2023-04-11 01:27:28 +0530

    Fixed nested tag fetch function to not include file suffix

--- util.py
+++ util.py
@@ -10,4 +10,7 @@ def get_nested_tag(file_path, root):
     """
     relative_file_path = Path(file_path).relative_to(Path(root))
     tag_name = str(relative_file_path).replace(' ', '').replace('\\', '/')
+    suffix_index = tag_name.find('.')
+    if suffix_index > -1:
+        return tag_name[:suffix_index]
     return tag_name
